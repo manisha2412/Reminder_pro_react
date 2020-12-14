b35ea6fdfd9ff55c3387162a1d70dc2e715e5c1e
manisha2412 <manishasharma.155@gmail.com>
2020-12-14 16:33:03 +0530

    initial code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  App from './components/App';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducer from './reducers';
+import './index.css'
+import AddTrainComponent from "./components/AddTrainComponent";
+import Home from "./components/Home";
 
-const store =createStore(reducer);
+const store = createStore(reducer);
 ReactDOM.render(<Provider store={store}>
-<App/>
-</Provider>,document.getElementById('root')
+        <Home/>
+
+    </Provider>, document.getElementById('root')
 )
 
 
